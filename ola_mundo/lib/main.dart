6375d07edc2ba0e3269aa57b21ecedda122b123e
Matheus Hora <matheus11hora@gmail.com>
2022-06-21 23:16:58 -0300

    Novos

--- ola_mundo/lib/main.dart
+++ ola_mundo/lib/main.dart
@@ -1,21 +1,6 @@
 import 'package:flutter/material.dart';
+import 'app_widget.dart';
 
 main() {
   runApp(AppWidget(title: "Me paga luisão"));
-}
-
-class AppWidget extends StatelessWidget {
-  final String title;
-
-  const AppWidget({super.key, required this.title});
-
-  @override
-  Widget build(BuildContext context) => Container(
-          child: Center(
-        child: Text(
-          title,
-          textDirection: TextDirection.ltr,
-          style: TextStyle(color: Colors.white, fontSize: 64.0),
-        ),
-      ));
 }
