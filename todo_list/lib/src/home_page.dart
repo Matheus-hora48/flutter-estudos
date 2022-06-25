8fb0347b2ee193d8365b8cf532ebcf7d66fc3146
Matheus Hora <matheus11hora@gmail.com>
2022-06-25 14:53:42 -0300

    Novos

--- todo_list/lib/src/home_page.dart
+++ todo_list/lib/src/home_page.dart
@@ -9,6 +9,14 @@ class HomePage extends StatelessWidget {
       appBar: AppBar(
         title: Text('List Todo\'s'),
       ),
+      body: ListView.builder(
+        itemCount: 20,
+        itemBuilder: (context, index) {
+          return ListTile(
+            title: Text('item $index'),
+          );
+        },
+      ),
     );
   }
 }
