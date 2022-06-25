11367eb164fc0d302677017c73aaa75c1af770bc
Matheus Hora <matheus11hora@gmail.com>
2022-06-25 13:09:01 -0300

    Novos

--- sync_asunc/bin/sync_asunc.dart
+++ sync_asunc/bin/sync_asunc.dart
@@ -1,43 +1,14 @@
-import 'dart:convert';
-
-import 'package:http/http.dart' as http;
-
-void main() async {
-  final todo = await fetch();
-  print(todo.userId);
+void main() {
+  print(stringToInt('1'));
 }
 
-Future<Todo> fetch() async {
-  var url = Uri.parse('https://jsonplaceholder.typicode.com/todos/1');
-  var response = await http.get(url);
-  var json = jsonDecode(response.body);
-  var todo = Todo.fromJson(json);
-  return todo;
-}
-
-class Todo {
-  final String? title;
-  final int? id;
-  final int? userId;
-  final bool? completed;
-
-  Todo({this.title, this.id, this.userId, this.completed});
-
-  factory Todo.fromJson(Map json) {
-    return Todo(
-      id: json['id'],
-      title: json['title'],
-      completed: json['completed'],
-      userId: json['userId'],
-    );
+int stringToInt(String text) {
+  int value;
+  try {
+    int value = int.parse(text);
+  } catch (e) {
+    value = 0;
   }
 
-  Map toJson() {
-    return {
-      'id': id,
-      'title': title,
-      'completed': completed,
-      'userId': userId,
-    };
-  }
+  return value;
 }
