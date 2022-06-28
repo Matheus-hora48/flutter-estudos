bfcc474ab97cede8c6480d84b784d486bd271780
Matheus Hora <matheus11hora@gmail.com>
2022-06-27 21:43:17 -0300

    Novos

--- inicio/lib/main.dart
+++ inicio/lib/main.dart
@@ -1,35 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'myapp.dart';
+
 void main() {
   runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: HomeWidget(),
-    );
-  }
-}
-
-class HomeWidget extends StatelessWidget {
-  const HomeWidget({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          title: Text("Home"),
-        ),
-        body: Center(
-          child: Text('Teste'),
-        ));
-    floatingActionButton:
-    FloatingActionButton(
-      onPressed: () {},
-    );
-  }
 }
