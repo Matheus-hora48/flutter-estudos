d1faeca526b261868313c76078dac5ef173df6f9
Matheus Hora <matheus11hora@gmail.com>
2022-06-29 22:03:55 -0300

    Novos

--- na_pratica/lib/main.dart
+++ na_pratica/lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(new MyApp());
+void main() => runApp(new MaterialApp(
+      home: new MyApp(),
+    ));
 
 class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
